f5903cc9e49fec5d3a3963998515520c9fc3650b
barmin15 <115711430+barmin15@users.noreply.github.com>
2023-11-28 14:10:49 +0100

    expose: self

--- RestaurantDemoServer/src/main/java/com/restaurant/restaurantdemoserver/security/SecurityConfig.java
+++ RestaurantDemoServer/src/main/java/com/restaurant/restaurantdemoserver/security/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers(HttpMethod.POST, "/api/auth/login", "/api/auth/register", "/api/auth/kitchen/login").permitAll()
+                        .requestMatchers("/swagger-ui/**","/v3/**", "/api/auth/login", "/api/auth/register", "/api/auth/kitchen/login").permitAll()
                         .anyRequest().authenticated()
                 );
         return http.build();
